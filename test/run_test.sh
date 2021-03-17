a2fdcfcdf0a842cdb511446e1aef663c0dcc39e9
William Savran <wsavran@usc.edu>
2021-03-16 19:13:08 -0700

    added multiple magnitude to number test

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,18 +1,5 @@
 #! /bin/bash
 
 echo "Running test simulation for /project/scec_608/wsavran/csep/u3etas_weekly/2020_07_22-weekly-1986-present-full_td/Start2011_08_17_ntest_result.json"
-python ../u3etas_post_processing.py 0 ../full_td/2020_07_22-weekly-1986-present-full_td_manifest.txt /project/scec_608/wsavran/git/weekly_u3etas_processing/manual_testing
+python ../u3etas_post_processing.py 0 ../full_td/2020_07_22-weekly-1986-present-full_td_manifest.txt /project/scec_608/wsavran/git/weekly_u3etas_processing/test
 
-echo "Checking output from this run against stored results"
-if ! cmp Start2011_08_17_ntest_result.json /project/scec_608/wsavran/csep/u3etas_weekly/2020_07_22-weekly-1986-present-full_td/Start2011_08_17_ntest_result.json 
-then
-    echo "Warning: Results differ for n-test you should re-compute the simulation results"
-else
-    echo "n-test results match."
-fi
-if ! cmp Start2011_08_17_stest_result.json /project/scec_608/wsavran/csep/u3etas_weekly/2020_07_22-weekly-1986-present-full_td/Start2011_08_17_stest_result.json 
-then
-    echo "Warning: Results differ for s-test you should re-compute the simulation results"
-else
-    echo "s-test results match."
-fi
--- u3etas_post_processing.py
+++ u3etas_post_processing.py
@@ -9,6 +9,8 @@ from csep import load_catalog_forecast, load_catalog, write_json
 from csep.core import regions, catalog_evaluations
 from csep.utils.constants import SECONDS_PER_WEEK
 from csep.utils.file import mkdirs
+from csep.utils.stats import get_quantiles
+from csep.models import CatalogNumberTestResult
 
 config = {
     'version': 2,
@@ -17,15 +19,53 @@ config = {
     'forecast_duration_millis': SECONDS_PER_WEEK * 1000,
     'region_information': {
         'name': 'california_relm_region',
-        'min_mw': 3.5,
+        'min_mw': 2.6,
         'max_mw': 8.0,
-        'dmw': 0.2
+        'dmw': 0.1
     },
     'catalog_path': '/project/scec_608/wsavran/git/weekly_u3etas_processing/comcat-2021-02-12-unfiltered.csv',
     'job_idx': int(sys.argv[1]),
     'forecast_path': '',
     'forecast_dir': '',
 }
+
+def number_test_multiple_mag(forecast, observed_catalog, mags=[2.6, 3.0, 3.5, 4.0, 4.5, 5.0]):
+    """ Performs the number test using space-magnitude binning to efficiently compute 
+        number test over multiple magnitudes. 
+    """
+    # compute gridded event counts 
+    results = []
+    obs_gc = observed_catalog.spatial_magnitude_counts()
+    first = True
+    fore_counts = []
+    for cat in forecast:
+        fore_gc = cat.spatial_magnitude_counts()
+        cat_counts = []
+        for mag in mags:
+            mag_idx = np.where(cat.region.magnitudes >= mag)[0]
+            fc_count = fore_gc[:,mag_idx].sum()
+            if first:
+                obs_gc = fore_gc[:,mag_idx].sum()
+                first = False
+            cat_counts.append(fc_count)
+        fore_counts.append(cat_counts)
+    # prepare results
+    fore_counts = np.array(fore_counts)
+    for idx, mag in enumerate(mags):
+        delta_1, delta_2 = get_quantiles(fore_counts[:,idx], obs_gc)
+        # prepare result
+        result = CatalogNumberTestResult(test_distribution=fore_counts[:,idx],
+                                         name='Catalog N-Test',
+                                         observed_statistic=obs_gc,
+                                         quantile=(delta_1, delta_2),
+                                         status='normal',
+                                         obs_catalog_repr=str(observed_catalog),
+                                         sim_name=forecast.name,
+                                         min_mw=mag,
+                                         obs_name=observed_catalog.name)
+        results.append(result)
+    return results
+        
 
 def create_space_magnitude_region(name, min_mw, max_mw, dmw):
     # if we include this in the package we can expand
@@ -154,6 +194,24 @@ def process_ucerf3_forecast(config):
         print(f"Writing outputs to {config['ntest_path']}.")
     except IOError:
         print("Unable to write n-test result.")
+
+    # Compute number test over multiple magnitudes
+    print("Computing number test over multiple magnitudes")
+    ntest_results = number_test_multiple_mag(forecast, eval_catalog)
+    config['ntest_paths'] = []
+    for r in ntest_results:
+        min_mw = r.min_mw
+        ntest_path = os.path.join(
+            config['output_dir'],
+            create_output_filepath(config['forecast_dir'], 'ntest_result_' + str(min_mw).replace('.','p') + '.json')
+        )
+        try:
+            write_json(ntest_result, ntest_path)
+            config['ntest_paths'].append(ntest_path)
+            print(f"Writing outputs to {ntest_path}.")
+        except IOError:
+            print("Unable to write n-test result.")
+
 
     # Compute and store magnitude test
     print("Computing magnitude-test on forecast.")
